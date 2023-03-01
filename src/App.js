be467a59736595e37c959f8b8e75ae5c3eb005fe
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-03-01 12:31:58 +0800

    some adding in firebase storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { database, storage } from "./firebase";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const STORAGE_FILES_KEY = "images/";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +19,9 @@ class App extends React.Component {
     // When Firebase changes, update local state, which will update local UI
     this.state = {
       messages: [],
+      message: "",
+      fileInputFile: null,
+      fileInputValue: "",
     };
   }
 
@@ -29,16 +38,46 @@ class App extends React.Component {
   }
 
   // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  writeData = (url) => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    set(newMessageRef, {
+      message: this.state.message,
+      date: new Date().toLocaleString(),
+      url: url,
+    });
+  };
+
+  submit = () => {
+    const storageRefFull = storageRef(
+      storage,
+      STORAGE_FILES_KEY + this.state.fileInputFile.name
+    );
+
+    uploadBytes(storageRefFull, this.state.fileInputFile).then((snapshot) => {
+      console.log(snapshot);
+      console.log(this.state);
+      getDownloadURL(storageRefFull, this.state.fileInputFile.name).then(
+        (url) => {
+          console.log("URL", url);
+
+          this.writeData(url);
+        }
+      );
+    });
   };
 
   render() {
     // Convert messages in state to message JSX elements to render
     let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val}</li>
+      <li key={message.key}>
+        {message.val.message} - {message.val.date}
+        {message.val.url ? (
+          <img src={message.val.url} alt={message.val.url} />
+        ) : (
+          "No url"
+        )}
+      </li>
     ));
     return (
       <div className="App">
@@ -48,7 +87,22 @@ class App extends React.Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
+          <input
+            type="text"
+            value={this.state.message}
+            onChange={(e) => this.setState({ message: e.target.value })}
+          />
+          <input
+            type="file"
+            value={this.state.fileInputValue}
+            onChange={(e) => {
+              this.setState({
+                fileInputFile: e.target.files[0],
+                fileInputValue: e.target.file,
+              });
+            }}
+          />
+          <button onClick={this.submit}>Send</button>
           <ol>{messageListItems}</ol>
         </header>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
@@ -9,7 +10,7 @@ const firebaseConfig = {
   // The value of `databaseURL` depends on the location of the database
   databaseURL: process.env.REACT_APP_REALTIME_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGE_SENDER_ID,
   appId: process.env.REACT_APP_ID,
 };
@@ -19,3 +20,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 // Get a reference to the database service and export the reference for other modules
 export const database = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
